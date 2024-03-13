b261b9a432a494910d0c26dabe0a4828105cb0c1
Theo <143622653+Speender@users.noreply.github.com>
2024-03-13 22:42:18 +0800

    Update and rename MovieTicketPricing.java to MatineeTicketPricing.java

--- chapter 16/MatineeTicketPricing.java
+++ chapter 16/MatineeTicketPricing.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MovieTicketPricing {
+public class MatineeTicketPricing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
